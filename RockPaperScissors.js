0a284349c1bac90363f19f307ce8aa8d4f633297
Zachary Lai <zachlai52@gmail.com>
2024-02-25 20:43:56 -0800

    Finished playGame function. Now plays five rounds and prints in console whether user won or not.  No GUI now, all in console.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -12,4 +12,63 @@ function getComputerChoice() {
     return "scissors";
 }
 
-console.log(getComputerChoice());
+function playRound( playerSelection, computerSelection, w, l ) {
+    player = playerSelection.toLowerCase()
+    computer = computerSelection.toLowerCase()
+    s = ""
+    result = 0
+    if(  player == 'rock' )   {
+        if( computer == 'rock' ) {
+            console.log("Tie! Rock ties Rock.");
+        }
+        else if( computer == 'paper' )  {
+            console.log("You lose! Paper beats rock.");
+        }
+        else if( computer == 'scissors' )   {
+            console.log("You win! Rock beats scissors.");
+            result = 1;
+        }
+    }
+    else if( player == 'paper' )    {
+        if( computer == 'rock' ) {
+            console.log("You win! Rock beats scissors.").
+            result = 1;
+        }
+        else if( computer == 'paper' )  {
+            console.log("Tie! Paper ties paper.");
+        }
+        else if( computer == 'scissors' )   {
+            console.log("You lose! Scissors beats paper.");
+        }
+    }
+    if( computer == 'rock' ) {
+        console.log("You lose! Rock beats scissors.");
+    }
+    else if( computer == 'paper' )  {
+        console.log("You win! Scissors beats paper.");
+        result = 1;
+    }
+    else{
+        console.log("Tie! Scissors ties scissors.");
+    }
+    return result;
+}
+
+function playGame()  {
+    let win = 0;
+    let lose = 0
+    for( let i = 0; i < 5; i++ )    {
+        let playerSelection = prompt("Enter 'r'(rock), 'p'(paper), or 's'(scissors):");
+        const computerSelection = getComputerChoice();
+        res = playRound( playerSelection, computerSelection, win, lose );
+    }
+    if( res > 3 )   {
+        console.log("You won!");
+    }
+    else{
+        console.log("You lost!  Hahahahahahahaha")
+    }
+}
+
+let count = 0;
+console.log(playGame());
